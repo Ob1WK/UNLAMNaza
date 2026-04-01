345cd7caf39131f6b1fe28fec9028a0a7d93453a
gabyanibaldi <anibaldigaby@gmail.com>
2026-04-01 16:30:49 -0300

    Fix: disable cache for home

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,19 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   allowedDevOrigins: ["localhost", "127.0.0.1", "192.168.89.165"],
+  async headers() {
+    return [
+      {
+        source: "/",
+        headers: [
+          {
+            key: "Cache-Control",
+            value: "no-store",
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
